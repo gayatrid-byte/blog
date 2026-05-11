e75990c3b7c84aa3775375a5f78f1348f03bf701
Gayatri Sudhir Deshpande <gayatrida05@gmail.com>
2026-05-11 15:57:31 +0530

    Update admin.js

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -13,21 +13,21 @@ const jwtSecret = process.env.JWT_SECRET;
  * 
  * Check Login
 */
-const authMiddleware = (req, res, next ) => {
-  const token = req.cookies.token;
+// const authMiddleware = (req, res, next ) => {
+//   const token = req.cookies.token;
 
-  if(!token) {
-    return res.status(401).json( { message: 'Unauthorized'} );
-  }
+//   if(!token) {
+//     return res.status(401).json( { message: 'Unauthorized'} );
+//   }
 
-  try {
-    const decoded = jwt.verify(token, jwtSecret);
-    req.userId = decoded.userId;
-    next();
-  } catch(error) {
-    res.status(401).json( { message: 'Unauthorized'} );
-  }
-}
+//   try {
+//     const decoded = jwt.verify(token, jwtSecret);
+//     req.userId = decoded.userId;
+//     next();
+//   } catch(error) {
+//     res.status(401).json( { message: 'Unauthorized'} );
+//   }
+// }
 
 
 /**
