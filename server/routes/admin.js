73caf404f72a6746de229c49cb07d9d5aae94a34
Gayatri Sudhir Deshpande <gayatrida05@gmail.com>
2026-05-11 15:57:01 +0530

    Update admin.js

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -82,7 +82,7 @@ router.post('/admin', async (req, res) => {
  * GET /
  * Admin Dashboard
 */
-router.get('/dashboard', authMiddleware, async (req, res) => {
+router.get('/dashboard', async (req, res) => {
   try {
     const locals = {
       title: 'Dashboard',
@@ -107,7 +107,7 @@ router.get('/dashboard', authMiddleware, async (req, res) => {
  * GET /
  * Admin - Create New Post
 */
-router.get('/add-post', authMiddleware, async (req, res) => {
+router.get('/add-post', async (req, res) => {
   try {
     const locals = {
       title: 'Add Post',
@@ -131,7 +131,7 @@ router.get('/add-post', authMiddleware, async (req, res) => {
  * POST /
  * Admin - Create New Post
 */
-router.post('/add-post', authMiddleware, async (req, res) => {
+router.post('/add-post', async (req, res) => {
   try {
     try {
       const newPost = new Post({
@@ -155,7 +155,7 @@ router.post('/add-post', authMiddleware, async (req, res) => {
  * GET /
  * Admin - Create New Post
 */
-router.get('/edit-post/:id', authMiddleware, async (req, res) => {
+router.get('/edit-post/:id', async (req, res) => {
   try {
 
     const locals = {
@@ -182,7 +182,7 @@ router.get('/edit-post/:id', authMiddleware, async (req, res) => {
  * PUT /
  * Admin - Create New Post
 */
-router.put('/edit-post/:id', authMiddleware, async (req, res) => {
+router.put('/edit-post/:id', async (req, res) => {
   try {
 
     await Post.findByIdAndUpdate(req.params.id, {
@@ -245,7 +245,7 @@ router.post('/register', async (req, res) => {
  * DELETE /
  * Admin - Delete Post
 */
-router.delete('/delete-post/:id', authMiddleware, async (req, res) => {
+router.delete('/delete-post/:id', async (req, res) => {
 
   try {
     await Post.deleteOne( { _id: req.params.id } );
